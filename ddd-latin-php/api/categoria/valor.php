248a2bde7ef3646df83a724a7c996838fd07425e
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2022-10-07 23:31:22 +0200

    variable para el servidor

--- ddd-latin-php/api/categoria/valor.php
+++ ddd-latin-php/api/categoria/valor.php
@@ -4,19 +4,25 @@ include '../keypass.php';
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
-	if (isset($_POST["idCategory"]))
+	if (isset($_GET["idCategory"]))
 	{
 		
-		$data = stream_get_contents(STDIN);
+		$data = file_get_contents('php://input');
 		
 		
-		$idCategory=$_POST["idCategory"];
+		$idCategory=$_GET["idCategory"];
 		
-		$ch = curl_init("http://clavy.fdi.ucm.es:8080/Clavy/rest/odamobil/getCat/".idCategory."?keyclavy=".$PassKey); 
+		$ch = curl_init("http://clavy.fdi.ucm.es:8080/Clavy/rest/odamobil/getCat/".$idCategory."/valor?keyclavy=".$PassKey); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_HTTPHEADER,
+    array(
+        'Content-Type:application/json',
+        'Content-Length: ' . strlen($data)
+    )
+	);
 
 
 $data = curl_exec($ch);
@@ -35,26 +41,6 @@ else
 	echo "{}";
 	die;
 }
-
-
-
-
-
-
-$ch = curl_init("http://clavy.fdi.ucm.es:8080/Clavy/rest/odamobil/getCat/" + idCategory +  "?keyclavy=".$PassKey); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-		curl_setopt($tuCurl, CURLOPT_POST, 1);
-
-
-$data = curl_exec($ch);
-curl_close($ch);
-
-echo $data;
 
 
 
--- ddd-latin-php/api/documentos/categoria/valor.php
+++ ddd-latin-php/api/documentos/categoria/valor.php
@@ -4,15 +4,24 @@ include '../../keypass.php';
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
-	if (isset($_POST["idCategory"])&&isset($_POST["val"]))
+	if (isset($_GET["idCategory"])&&isset($_GET["val"]))
 	{
-		$idCategory=$_POST["idCategory"];
-		$val=$_POST["val"];
+		$idCategory=$_GET["idCategory"];
+		$val=$_GET["val"];
+		
+		$data = file_get_contents('php://input');
 		
 		$ch = curl_init("http://clavy.fdi.ucm.es:8080/Clavy/rest/odamobil/getCatDoc/".$idCategory."/".$val."?keyclavy=".$PassKey); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_HTTPHEADER,
+    array(
+        'Content-Type:application/json',
+        'Content-Length: ' . strlen($data)
+    )
+);
 
 
 $data = curl_exec($ch);
